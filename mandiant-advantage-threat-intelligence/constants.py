d86477734f86f6e6341d49735a51f19467eb8376
cs-shubham-morankar <smorankar@fortinet.com>
2023-09-27 11:07:01 +0530

    Initial commit

---
+++ mandiant-advantage-threat-intelligence/constants.py
@@ -0,0 +1,10 @@
+""" Copyright start
+  Copyright (C) 2008 - 2023 Fortinet Inc.
+  All rights reserved.
+  FORTINET CONFIDENTIAL & FORTINET PROPRIETARY SOURCE CODE
+  Copyright end """
+
+SORT_ORDER = {
+    'Ascending': 'asc',
+    'Descending': 'desc'
+}
